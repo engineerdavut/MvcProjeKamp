9df87f7e4d7ca8b05dc5858b585c98783b9e83ed
Davut Aktaş <engineerdavut@gmail.com>
2021-05-24 22:07:26 +0300

    40odev

--- MvcProjeKamp/Controllers/StaticsController.cs
+++ MvcProjeKamp/Controllers/StaticsController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,18 @@ namespace MvcProjeKamp.Controllers
     public class StaticsController : Controller
     {
         // GET: Statics
+        
+        Context context = new Context();
         public ActionResult Index()
         {
-            return View();
+            ViewBag.d1 = context.Categories.Count().ToString();
+            ViewBag.d2 = context.Headings.Count(x => x.Category.CategoryName == "Yazılım").ToString();
+            ViewBag.d3 = (from x in context.Writers select x.WriterFirstName.IndexOf("a")).Distinct().Count().ToString();
+            ViewBag.d4 = context.Categories.Where(u => u.CategoryId == (context.Headings.GroupBy(x => x.CategoryId)
+           .OrderByDescending(z => z.Count()).Select(y => y.Key).FirstOrDefault())).Select(k => k.CategoryName).FirstOrDefault();
+            ViewBag.d5 = (context.Categories.Count(x => x.CategoryStatus == true) - context.Categories.Count(x => x.CategoryStatus == false)).ToString();
+            return View(ViewBag);
         }
+
     }
 }
